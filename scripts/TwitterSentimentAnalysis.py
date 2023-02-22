638e29fb54a8786fe1d5015eabc86a4eb39aea7d
Parker Ferguson <parkcity.pf@gmail.com>
2023-02-21 23:27:46 -0800

    Added child process to to indexjs. Process generates pdf and returns path to file.

--- scripts/TwitterSentimentAnalysis.py
+++ scripts/TwitterSentimentAnalysis.py
@@ -1,3 +1,4 @@
+import sys
 import tweepy
 import os as os
 import numpy as np
@@ -20,13 +21,10 @@ def remove_all_values(myList, valueForRemoval):
 
 def main():
     # Get user input for Sentiment analysis text
-    keyword = input("Enter text for Twitter Sentiment Analaysis: ")
-
+    keyword = sys.argv[1]
     # Pull Tweets on keyword
     tweets = api.search(keyword, count=1000)
-
     polarity = []
-    subjectivity = []
 
     # Perform analysis and create lists for manipulation (polarity/subjectivity)
     for tweet in tweets:
@@ -34,44 +32,20 @@ def main():
 
         # Create list of polarity & subjectivity for mean
         polarity.append(analysis.sentiment.polarity)
-        subjectivity.append(analysis.sentiment.subjectivity)
+        # subjectivity.append(analysis.sentiment.subjectivity)
 
     # Remove 0.0 (neutral) polarity & subjectivity
     polarity = remove_all_values(polarity, 0.0)
-    subjectivity = remove_all_values(subjectivity, 0.0)
 
-    # Output
-    print("Sentiment average Polarity: " + str(np.mean(polarity)))
-    print("Sentiment average Subjectivity: " + str(np.mean(subjectivity)))
-
-    # Generate histograms
+    # Generate polarity istogram
     polarityDf = pd.DataFrame(
         polarity, columns=["Polarity - '" + keyword + "'"])
-    subjectivityDf = pd.DataFrame(
-        subjectivity, columns=["Subjectivity - '" + keyword + "'"])
+    polarityDf.hist(color="orange")
+    plt.savefig('./assets/polarity_' + keyword + '.pdf')
 
-    # Sentiment (both polarity and subjectivity) in single DataFrame - scatter plot
-    #sentimentDF = pd.DataFrame({"Polarity": polarity, "Subjectivity": subjectivity})
-    #sentimentDF.plot.scatter(x="Polarity", y="Subjectivity", color="blue")
+    file = 'polarity_' + keyword + '.pdf'
 
-    polarityDf.hist(color="blue")
-    plt.savefig('polarity_' + keyword + '.pdf')
-    subjectivityDf.hist(color="orange")
-    plt.savefig('subjectivity_' + keyword + '.pdf')
-
-    file1 = os.getcwd() + 'polarity_' + keyword + '.pdf'
-    file2 = os.getcwd() + 'subjectivity_' + keyword + '.pdf'
-
-    # File names for return data
-    files = [file1, file2]
-
-    #showHistograms = input("Show Historgrams? Y or N ")
-    # if(showHistograms.lower() == ("y" or "yes")):
-    # plt.show()
-    # else:
-    # exit()
-
-    return files
+    print(file)
 
 
 main()
--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,27 @@
 const keys = require('./keys');
-const redis = require('redis');
+// const redis = require('redis');
+const {spawn} = require('child_process');
 
-const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+// const redisClient = redis.createClient({
+//     host: keys.redisHost,
+//     port: keys.redisPort,
+//     retry_strategy: () => 1000
+// });
+
+// const sub = redisClient.duplicate();//Redis required 2 conns
+
+let keyword = 'trump';
+
+// spawn new child process to call the python script
+const python = spawn('python3', ['../scripts/TwitterSentimentAnalysis.py', keyword]);
+// collect data from script
+python.stdout.on('data', (data) => {
+    console.log('Pipe data from python script ...');
+    data = data.toString()
+    console.log(data);
+
 });
-
-const sub = redisClient.duplicate();//Redis required 2 conns
-
-function myFunction(nidex)
+// in close event we are sure that stream from child process is closed
+python.on('close', (code) => {
+console.log(`child process close all stdio with code ${code}`);
+});
