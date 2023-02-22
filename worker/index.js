3f902fa6c600144de0154468c3f49ab5a5177a2d
Parker Ferguson <parkcity.pf@gmail.com>
2023-02-21 23:41:15 -0800

    Added Redis client connection to worker. Insert anytime new value is returned from child process.

--- worker/index.js
+++ worker/index.js
@@ -1,27 +1,38 @@
 const keys = require('./keys');
-// const redis = require('redis');
+const redis = require('redis');
 const {spawn} = require('child_process');
 
-// const redisClient = redis.createClient({
-//     host: keys.redisHost,
-//     port: keys.redisPort,
-//     retry_strategy: () => 1000
-// });
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
 
-// const sub = redisClient.duplicate();//Redis required 2 conns
+const sub = redisClient.duplicate();//Redis required 2 conns
 
+//ToDo: Input keyword from client UI 
 let keyword = 'trump';
 
-// spawn new child process to call the python script
-const python = spawn('python3', ['../scripts/TwitterSentimentAnalysis.py', keyword]);
-// collect data from script
-python.stdout.on('data', (data) => {
-    console.log('Pipe data from python script ...');
-    data = data.toString()
-    console.log(data);
+function sentimentAnalysis() {
+    // spawn new child process to call the python script
+    const python = spawn('python3', ['../scripts/TwitterSentimentAnalysis.py', keyword]);
+    // collect data from script
+    python.stdout.on('data', (data) => {
+        console.log('Pipe data from python script ...');
+        data = data.toString()
+        console.log(data);
 
-});
-// in close event we are sure that stream from child process is closed
-python.on('close', (code) => {
-console.log(`child process close all stdio with code ${code}`);
-});
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    });
+    return data;
+}
+
+//function data to Redis instance
+sub.on('message', () => {
+    redis.redisClient.hset('values', message, sentimentAnalysis(message));
+})
+
+sub.subscribe('insert');
