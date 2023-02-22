19ad66df8914f9f3d6557e645458681339f03084
Parker Ferguson <parkcity.pf@gmail.com>
2023-02-21 18:27:36 -0800

    Added script to create sentiment analysis hist.

---
+++ scripts/TwitterSentimentAnalysis.py
@@ -0,0 +1,64 @@
+import tweepy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+from keys import *
+
+# Authentical with tweepy OAuth and Twitter API
+auth = tweepy.OAuthHandler(apiKey, apiSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+api = tweepy.API(auth)
+
+
+def remove_all_values(myList, valueForRemoval):
+    while(valueForRemoval in myList):
+        myList.remove(valueForRemoval)
+    return myList
+
+
+# Get user input for Sentiment analysis text
+keyword = input("Enter text for Twitter Sentiment Analaysis: ")
+
+# Pull Tweets on keyword
+tweets = api.search(keyword, count=1000)
+
+polarity = []
+subjectivity = []
+
+# Perform analysis and create lists for manipulation (polarity/subjectivity)
+for tweet in tweets:
+    analysis = TextBlob(tweet.text)
+
+    # Create list of polarity & subjectivity for mean
+    polarity.append(analysis.sentiment.polarity)
+    subjectivity.append(analysis.sentiment.subjectivity)
+
+# Remove 0.0 (neutral) polarity & subjectivity
+polarity = remove_all_values(polarity, 0.0)
+subjectivity = remove_all_values(subjectivity, 0.0)
+
+# Output
+print("Sentiment average Polarity: " + str(np.mean(polarity)))
+print("Sentiment average Subjectivity: " + str(np.mean(subjectivity)))
+
+# Generate histograms
+polarityDf = pd.DataFrame(polarity, columns=["Polarity - '" + keyword + "'"])
+subjectivityDf = pd.DataFrame(
+    subjectivity, columns=["Subjectivity - '" + keyword + "'"])
+
+# Sentiment (both polarity and subjectivity) in single DataFrame - scatter plot
+#sentimentDF = pd.DataFrame({"Polarity": polarity, "Subjectivity": subjectivity})
+#sentimentDF.plot.scatter(x="Polarity", y="Subjectivity", color="blue")
+
+
+polarityDf.hist(color="blue")
+plt.savefig('polarity_' + keyword + '.pdf')
+subjectivityDf.hist(color="orange")
+plt.savefig('subjectivity_' + keyword + '.pdf')
+
+#showHistograms = input("Show Historgrams? Y or N ")
+# if(showHistograms.lower() == ("y" or "yes")):
+# plt.show()
+# else:
+# exit()
---
+++ worker/index.js
@@ -0,0 +1,12 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();//Redis required 2 conns
+
+function myFunction(nidex)
