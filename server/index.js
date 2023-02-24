b59fd0a0e5e95beccc669615ca903a7735bf8dbd
Parker Ferguson <parkcity.pf@gmail.com>
2023-02-23 21:46:09 -0800

    Server - index.js complete with express api routing. Postgres and Redis clients. New keywords added to redis for calculation, output to save to postgres.

--- server/index.js
+++ server/index.js
@@ -28,4 +28,48 @@ pgClient.on("connect", (client) => {
     client
       .query("CREATE TABLE IF NOT EXISTS values (keyword varchar(255) NOT NULL)")
       .catch((err) => console.error(err));
+});
+
+//Redis Client
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redistClient.duplicate();
+
+//Express route handlers
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  //Redis library doesn't have promist support, callback instead of async/await above
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
   });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  //Add to redis 
+  redisClient.hset('values', index, 'Nothing yet');
+  redisPublisher('insert', index);
+  //Store keyword perminantly in postgres database
+  pgClient.query('INSERT INTO values(varchar) VALUES($1)', [index]);
+
+  res.send({ working: true });
+}); 
+
+app.listen(5000, err => {
+  console.log('Listening')
+});
